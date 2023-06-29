c39711342ac98f1ba32d925dad610e545ab7a146
Benson <mark.up1109@gmail.com>
2023-06-29 12:12:07 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
-import React, { useState } from 'react';
-import { Form, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import FormSearch from './FormSearch';
-import Footer from './Footer';
+import GetLocation from './GetLocation';
 
 function App() {
+  const [isSearchFieldOpen, setSearchFieldOpen] = useState(false);
+  const [userLocation, setUserLocation] = useState('');
+
+  const handleSearchButtonClick = () => {
+    setSearchFieldOpen(true);
+  };
+
+  const handleCloseButtonClick = () => {
+    setSearchFieldOpen(false);
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation(`${latitude}, ${longitude}`);
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+        }
+      );
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  }, []);
 
   return (
-    <div className="h-screen w-full flex-col flex items-center justify-center overflow-x-hidden overflow-y-hidden" style={{ background: 'url(https://cdn.wallpapersafari.com/93/77/rtZdDG.jpg)', backgroundSize: 'cover' }}>
-      <div className="w-[400px] h-[300px] backdrop-blur-sm flex justify-center items-center flex-col gap-8">
-        <h1 className='text-black bg-white py-2 px-6 text-3xl'>Weather App</h1>
-        <FormSearch/>
+    <div className='w-full'>
+      <header className='w-full h-10 flex justify-between p-6'>
+        <div>
+          <p className="text-xl">
+            <Link to="/react-weather">WeatherApp</Link>
+          </p>
+        </div>
+        <div>
+          <button className='searchBtn' onClick={handleSearchButtonClick}>
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+        {isSearchFieldOpen && (
+          <div className='searchField absolute w-full bg-white z-[9999] h-full inset-0 flex justify-center animate__animated animate__zoomInDown'>
+            <button
+              className='h-10 w-10 rounded-full shadow-md shadow-[#999] bg-white m-6 absolute top-0 right-0'
+              onClick={handleCloseButtonClick}
+            >
+              <i className="fa fa-close text-[#222]"></i>
+            </button>
+            <div className='w-[80%] h-20 flex items-center justify-center mt-20'>
+              <FormSearch />
+            </div>
+          </div>
+        )}
+      </header>
+      <div>
+        <GetLocation locationpos={userLocation} />
       </div>
-      
-      <Footer/>
     </div>
   );
 }
--- src/FormSearch.js
+++ src/FormSearch.js
@@ -1,36 +1,95 @@
-import React from 'react'
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-function FormSearch() {  
+import React, { useState, useEffect, useRef } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
+
+function FormSearch() {
   const [searchQuery, setSearchQuery] = useState('');
-const navigate = useNavigate();
+  const [suggest, setSuggest] = useState([]);
+  const navigate = useNavigate();
+  const inputRef = useRef(null);
 
-const handleSearch = (e) => {
-  e.preventDefault();
-  navigate(`/react-weather/check/${searchQuery}`);
-};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/react-weather/check/${searchQuery}`);
+    setSearchQuery('');
+    setSuggest([]);
+  };
 
-const handleChange = (e) => {
-  setSearchQuery(e.target.value);
-};
+  useEffect(() => {
+    const fetchData = async () => {
+      if (searchQuery.trim() === '') {
+        setSuggest([]);
+        return;
+      }
+
+      const options = {
+        method: 'GET',
+        url: 'https://weatherapi-com.p.rapidapi.com/search.json',
+        params: { q: searchQuery },
+        headers: {
+          'X-RapidAPI-Key': 'ef2f9f3508msh222e7bd07a8956fp1f91c7jsn27aa290318af',
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        setSuggest(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [searchQuery]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleBlur = () => {
+    setTimeout(() => {
+      setSuggest([]);
+    }, 200);
+  };
+
+  const handleSuggestionClick = () => {
+    setSearchQuery('');
+    setSuggest([]);
+    inputRef.current.focus();
+  };
 
   return (
-    <div className='flex justify-center w-[400px]'>
-        <form onSubmit={handleSearch} className='flex w-[80%] border-[1px] border-black'>
+    <div className="flex justify-center w-[400px] flex-col items-center">
+      <form onSubmit={handleSearch} className="flex w-[100%] flex-col relative">
+        <div className='w-full flex items-center justify-center'>
           <input
             type="text"
             name="check"
             placeholder="Search for a location..."
-            className="min-w-[60%] w-[300px] h-10 outline-none indent-4 focus:outline-none"
+            className="min-w-[60%] rounded-l-md w-[300px] h-14 outline-none indent-4 focus:outline-none bg-[#ebebeb]"
             value={searchQuery}
             onChange={handleChange}
-          required/>
-          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">
-            Search
+            onBlur={handleBlur}
+            ref={inputRef} autoComplete='off'
+            required
+          />
+          <button type="submit" className="bg-blue-500 rounded-r-md hover:bg-blue-700 text-white font-bold py-2 px-4 h-14 w-14">
+            <i className="fa fa-search"></i>
           </button>
-        </form>
+        </div>
+        {suggest.length > 0 && (
+          <div className="w-full bg-white suggestions absolute bottom-0 h-max top-14">
+            {suggest.map((location) => (
+              <Link to={`/react-weather/check/${location.name} ${location.region} ${location.country}`} onClick={handleSuggestionClick}>
+                <p key={location.id} className='line-clamp-1 animate__animated animate__bounceIn h-10 py-2 px-3 border-b-[1px] w-[90%] mx-auto border-[#ccc]'>{location.name}, {location.region}, {location.country}</p>
+              </Link>
+            ))}
+          </div>
+        )}
+      </form>
     </div>
-  )
+  );
 }
 
 export default FormSearch;
